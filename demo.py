d9df8c46ade8cf4b59fe29b88f1714d06464da5d
Wajdi Bsoul <wbsoul@hotmail.com>
2023-07-25 21:19:04 +1000

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,25 +1,16 @@
 import jwt
 from jwksutils import rsa_pem_from_jwk
+import requests
 
 # To run this example, follow the instructions in the project README
 
-# obtain jwks as you wish: configuration file, HTTP GET request to the endpoint returning them;
-jwks = {
-    "keys": [
-        {
-            "kid": "X5eXk4xyojNFum1kl2Ytv8dlNP4-c57dO6QGTVBwaNk",
-            "nbf": 1493763266,
-            "use": "sig",
-            "kty": "RSA",
-            "e": "AQAB",
-            "n": "tVKUtcx_n9rt5afY_2WFNvU6PlFMggCatsZ3l4RjKxH0jgdLq6CScb0P3ZGXYbPzXvmmLiWZizpb-h0qup5jznOvOr-Dhw9908584BSgC83YacjWNqEK3urxhyE2jWjwRm2N95WGgb5mzE5XmZIvkvyXnn7X8dvgFPF5QwIngGsDG8LyHuJWlaDhr_EPLMW4wHvH0zZCuRMARIJmmqiMy3VD4ftq4nS5s8vJL0pVSrkuNojtokp84AtkADCDU_BUhrc2sIgfnvZ03koCQRoZmWiHu86SuJZYkDFstVTVSR0hiXudFlfQ2rOhPlpObmku68lXw-7V-P7jwrQRFfQVXw"
-        }
-    ]
-}
+# obtain jwks from "https://login.microsoftonline.com/common/discovery/keys"). jwks_uri found in https://login.microsoftonline.com/common/.well-known/openid-configuration
+response = requests.get("https://login.microsoftonline.com/common/discovery/keys")
+jwks = response.json()['keys']
 
 # configuration, these can be seen in valid JWTs from Azure B2C:
-valid_audiences = ['d7f48c21-2a19-4bdb-ace8-48928bff0eb5'] # id of the application prepared previously
-issuer = 'https://ugrose.b2clogin.com/9c2984ff-d596-4e5c-8e74-672be7b592e3/v2.0/' # iss
+valid_audiences = ['yyyyyyy-yyyy-yyyy-yyyy-yyyyyyyyyyyy'] # id of the application (i.e managed Identity) receiving the token
+issuer = 'https://sts.windows.net/xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxx/' # iss
 
 
 class InvalidAuthorizationToken(Exception):
