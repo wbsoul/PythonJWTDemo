a1cf09dcee75af50b2ef026848280d73e39121db
Wajdi Bsoul <wbsoul@hotmail.com>
2023-07-25 21:28:25 +1000

    Update demo.py

--- demo.py
+++ demo.py
@@ -8,7 +8,7 @@ import requests
 response = requests.get("https://login.microsoftonline.com/common/discovery/keys")
 jwks = response.json()
 
-# configuration, these can be seen in valid JWTs from Azure B2C:
+# configuration, these can be seen in valid JWTs from Azure:
 valid_audiences = ['{Application-ID}'] # id of the application (i.e managed Identity) receiving the token
 issuer = 'https://sts.windows.net/{tenantid}/' # iss in JWT and issuer in https://login.microsoftonline.com/common/.well-known/openid-configuration
 
